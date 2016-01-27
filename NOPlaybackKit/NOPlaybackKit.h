82c974ffca013fcb5c572ee9446ff3d3aed65b8c
Jonathan Long <thejonathanlong@icloud.com>
2016-01-26 22:37:37 -0800

    Initial Commit

---
+++ NOPlaybackKit/NOPlaybackKit.h
@@ -0,0 +1,19 @@
+//
+//  NOPlaybackKit.h
+//  NOPlaybackKit
+//
+//  Created by Jonathan Long on 1/26/16.
+//  Copyright © 2016 Notorious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NOPlaybackKit.
+FOUNDATION_EXPORT double NOPlaybackKitVersionNumber;
+
+//! Project version string for NOPlaybackKit.
+FOUNDATION_EXPORT const unsigned char NOPlaybackKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NOPlaybackKit/PublicHeader.h>
+
+
